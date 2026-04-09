9e5a6e63bfd5e7b5b2d50364d551de89f3684a48
Ayaka Ito <rakkyo12ayaka@gmail.com>
2026-04-09 19:40:32 +0900

    ToDO詳細画面の表示機能を追加

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -36,4 +36,12 @@ public function store(Request $request)
     return redirect()->route('todo.index');
 }
 
+public function show($id)
+{
+    $model = new Todo();
+    $todo = $model->find($id);
+
+    return view('todo.show', ['todo' => $todo]);
+}
+
 }
--- src/routes/web.php
+++ src/routes/web.php
@@ -20,6 +20,9 @@
 // });
 
 Route::get('/todo', 'TodoController@index')->name('todo.index');
+
 Route::get('/todo/create', 'TodoController@create')->name('todo.create');
 
 Route::post('/todo', 'TodoController@store')->name('todo.store');
+
+Route::get('/todo/{id}', 'TodoController@show')->name('todo.show');
